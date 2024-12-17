2fcf65d3341491667a391f8aa7a3a85775bea80b
Noah Shan <luth595@gmail.com>
2024-12-17 22:21:58 +0530

    acts
    Changes to be committed:
    	modified:   src/pages/acts/Acts.jsx

--- src/pages/acts/Acts.jsx
+++ src/pages/acts/Acts.jsx
@@ -1,9 +1,31 @@
+import Header from "../../components/Header";
 function Acts() {
-    return (
-        <div>
-            acts
-        </div>
-    )
+  return (
+    <main
+      className="w-full min-h-screen bg-[url('./acts.jpg')] bg-cover
+         bg-center flex flex-col items-center justify-center p-3"
+    >
+      <Header
+        title="ක්‍රියා පොත."
+        values={[{book:"ක්‍රියා"}]}
+        content={
+          <>
+            <p className="sm:leading-9 md:leading-10 lg:leading-relaxed">
+              ක්‍රියා පොත ලෙස හැඳින්වෙන්නේ ක්‍රිස්තුස් වහන්සේ පොළොවේ සිටි
+              කාලයේදී එතුමා විසින් තෝරාගනු ලැබූ ගෝලයෝ තුළින් ශුද්ධ සභාවට
+              ශුද්ධාත්මයාණන් කරන කොට ගෙන ආරම්භවීම පිළිබඳව. ඒ අනුව ශුද්ධාත්මයාණන්
+              වහන්සේ වැගිරීමෙන් පසුව, යෙරුසලම තුළත්, කොරනේලියස්ගේ නිවසේ දෙවැනි
+              වර ශුද්ධාත්ම වැගිරීමෙන් පසු අන්‍ය ජාතිකයින් වෙතටත්, අවසානයේදී එවකට
+              අගනුවර ව තිබූ රෝමය වෙතටත් ශුද්ධ සභාවේ ව්‍යාප්තිය පිළිබඳව ව්‍යාත්මක
+              පැහැදිලි කිරීමක්ය.
+            </p>
+
+            <p className="text-amber-400 mt-6">ලූක්තුමා විසින් ලියා ඇත.</p>
+          </>
+        }
+      />
+    </main>
+  );
 }
 
-export default Acts
+export default Acts;
