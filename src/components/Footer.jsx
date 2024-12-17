b2c4938f5b065669ab13596e9025e0c704bd3390
Noah Shan <luth595@gmail.com>
2024-12-17 23:43:00 +0530

    changed footer padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className="h-44 bg-slate-950 border-t-2 border-slate-600 text-gray-100 flex items-center justify-center text-center leading-8">
+        <footer className="h-44 bg-slate-950 px-4 border-t-2 border-slate-600 text-gray-100 flex items-center justify-center text-center leading-8">
             Project for Systematic Theology Assignment.
             <br />
         2024 {new Date().getFullYear() > 2024 && `- ${new Date().getFullYear()}`} &copy;
